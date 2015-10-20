8338948ca7cd76f378ec5e5700aa174d0a3c58ed
Chris Wells <chris.wells@gmail.com>
2015-10-19 20:36:01 -0400

    Fixed spamBroadcast method.

--- teams/team046/RobotPlayer.java
+++ teams/team046/RobotPlayer.java
@@ -177,7 +177,7 @@ public class RobotPlayer {
     private static void spamBroadcast() throws GameActionException {
         while (power > GameConstants.BROADCAST_MAX_CHANNELS * GameConstants.BROADCAST_READ_COST) {
             for (int i = 0; i <= GameConstants.BROADCAST_MAX_CHANNELS; i++) {
-                if (i != supplierBuilderChannel
+                if (i != zergRushChannel
                         && i != supplierBuilderChannel
                         && rc.readBroadcast(i) != 0
                         && power - GameConstants.BROADCAST_READ_COST > GameConstants.BROADCAST_SEND_COST) {
