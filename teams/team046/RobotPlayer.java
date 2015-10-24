dfad2d092391d6b1faee5f814d8afc7800fe8fbf
Chris Wells <chris.wells@gmail.com>
2015-10-24 11:08:30 -0400

    Removed unused code.

--- teams/team046/RobotPlayer.java
+++ teams/team046/RobotPlayer.java
@@ -50,7 +50,6 @@ public class RobotPlayer {
                 return;
             }
             else {
-                //Robot[] meanies = rc.senseNearbyGameObjects(Robot.class, 14, rc.getTeam().opponent());
                 Robot[] myBuddies = rc.senseNearbyGameObjects(Robot.class, 14, rc.getTeam());
                 if (round > 500 && myBuddies.length > 20) {
                     rc.researchUpgrade(Upgrade.NUKE);
